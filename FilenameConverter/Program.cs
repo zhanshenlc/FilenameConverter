77fd704032243bdcbe7ad8db20cec92b374c3f91
Adam LI <zhanshenlc@live.com>
2020-02-28 11:03:00 +0000

    First commit

---
+++ FilenameConverter/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace FilenameConverter {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
