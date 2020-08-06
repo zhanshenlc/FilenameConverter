9da4a8c77da95dbec3e18c7a43db8fe0b17a0228
Adam LI <zhanshenlc@live.com>
2020-08-06 07:36:55 +0100

    Subtitle timeline editing feature

--- FilenameConverter/Program.cs
+++ FilenameConverter/Program.cs
@@ -8,25 +8,98 @@ namespace FilenameConverter {
         static void Main(string[] _) {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
             while (true) {
-                Console.WriteLine("Filename Converter\nPress 1 for songs\nPress 2 for anime\nPress Esc to exit");
+                Console.WriteLine("Filename Converter\nPress 1 for songs\nPress 2 for anime\n" +
+                    "Press 3 for editing subtitle timeline\nPress Esc to exit\n");
                 ConsoleKeyInfo keyInfo = Console.ReadKey();
+                Console.WriteLine();
                 if (keyInfo.Key == ConsoleKey.Escape) {
                     return;
                 } else if (keyInfo.Key == ConsoleKey.D1 || keyInfo.Key == ConsoleKey.NumPad1) {
-                    Console.WriteLine();
                     Song.ConvertSong();
                     break;
                 } else if (keyInfo.Key == ConsoleKey.D2 || keyInfo.Key == ConsoleKey.NumPad2) {
-                    Console.WriteLine();
                     Anime.ConvertAnime();
                     break;
+                } else if (keyInfo.Key == ConsoleKey.D3 || keyInfo.Key == ConsoleKey.NumPad3) {
+                    while (true) {
+                        Console.WriteLine("Subtitle TimeLine Editor\nPress 1 for 1 file\n" +
+                            "Press 2 for all files in folder\nPress 0 to return to last menu" +
+                            "\nPress Esc to exit\n");
+                        keyInfo = Console.ReadKey();
+                        if (keyInfo.Key == ConsoleKey.Escape) {
+                            return;
+                        } else if (keyInfo.Key == ConsoleKey.D1 || keyInfo.Key == ConsoleKey.NumPad1) {
+                            string filename;
+                            string currentDirectory = Directory.GetCurrentDirectory();
+                            while (true) {
+                                Console.WriteLine("Input the filename: ");
+                                filename = Console.ReadLine();
+                                filename = Path.Combine(currentDirectory, filename);
+                                if (Path.IsPathFullyQualified(filename)) {
+                                    break;
+                                }
+                            }
+                            SubtitleTime.EditSubtitleFile(filename, readTimeChange());
+                            break;
+                        } else if (keyInfo.Key == ConsoleKey.D2 || keyInfo.Key == ConsoleKey.NumPad2) {
+                            SubtitleTime.EditSubtitleFiles(readTimeChange());
+                            break;
+                        } else if (keyInfo.Key == ConsoleKey.D0 || keyInfo.Key == ConsoleKey.NumPad0) {
+                            break;
+                        } else {
+                            Console.WriteLine("Invalid Input\n");
+                        }
+                    }
                 } else {
-                    Console.WriteLine("Invalid Input");
+                    Console.WriteLine("Invalid Input\n");
                 }
             }
 
-            Console.WriteLine("Press any key to exit");
+            Console.WriteLine("\nPress any key to exit");
             Console.ReadKey();
+        }
+
+        private static TimeSpan readTimeChange() {
+            string sign, tmp;
+            while (true) {
+                Console.WriteLine("Positive or Negative? (Input 1 or 0): ");
+                sign = Console.ReadLine();
+                if (sign == "0" || sign == "1") {
+                    break;
+                }
+                Console.WriteLine("Invalid Input");
+            }
+
+            int seconds;
+            while (true) {
+                Console.WriteLine("Seconds (range from 0 to 10): ");
+                tmp = Console.ReadLine();
+                seconds = Convert.ToInt32(tmp);
+                if (seconds >= 0 && seconds <= 10) {
+                    break;
+                }
+                Console.WriteLine("Invalid Input");
+            }
+
+            int milliseconds;
+            while (true) {
+                Console.WriteLine("Milliseconds (range from 10 to 999 or 0): ");
+                tmp = Console.ReadLine();
+                milliseconds = Convert.ToInt32(tmp);
+                if (milliseconds >= 10 && milliseconds <= 999) {
+                    break;
+                }
+                if (milliseconds == 0) {
+                    break;
+                }
+                Console.WriteLine("Invalid Input");
+            }
+
+            if (sign == "0") {
+                return new TimeSpan(0, 0, 0, -seconds, milliseconds);
+            } else {
+                return new TimeSpan(0, 0, 0, seconds, milliseconds);
+            }
         }
     }
 }
---
+++ FilenameConverter/SubtitleTime.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace FilenameConverter {
+    class SubtitleTime {
+
+        private static string EditOneLine(string input, TimeSpan timeChange) {
+			// Match Dialogue
+			MatchCollection mc = Regex.Matches(input, @"^Dialogue: .*");
+			if (mc.Count != 1) {
+				return input;
+            }
+
+			// Match timeline
+			mc = Regex.Matches(input, @"\d:\d\d:\d\d.\d\d");
+			if (mc.Count != 2) {
+				throw new Exception("Invalid content format");
+            }
+			CultureInfo cultureInfo = new CultureInfo("zh-CN");
+			string startTimeStr = mc[0].ToString();
+			string endTimeStr = mc[1].ToString();
+			TimeSpan startTime = TimeSpan.Parse(startTimeStr, cultureInfo);
+			TimeSpan endTime = TimeSpan.Parse(endTimeStr, cultureInfo);
+
+			// Change timeline
+			string newStartTimeStr, newEndTimeStr;
+			startTime = startTime.Add(timeChange);
+			endTime = endTime.Add(timeChange);
+			if (TimeSpan.Compare(startTime, new TimeSpan(0, 0, 0)) == -1) {
+				startTime = new TimeSpan(0, 0, 0, 0);
+				newStartTimeStr = startTime.ToString().Remove(0, 1) + ".00";
+			} else {
+				newStartTimeStr = startTime.ToString().Remove(0, 1);
+				newStartTimeStr = Truncate(newStartTimeStr, 10);
+				if (newStartTimeStr.Length != 10) {
+					newStartTimeStr += ".00";
+                }
+            }
+			if (TimeSpan.Compare(endTime, new TimeSpan(0, 0, 0)) == -1) {
+				endTime = new TimeSpan(0, 0, 0, 0);
+				newEndTimeStr = endTime.ToString().Remove(0, 1) + ".00";
+			} else {
+				newEndTimeStr = endTime.ToString().Remove(0, 1);
+				newEndTimeStr = Truncate(newEndTimeStr, 10);
+				if (newEndTimeStr.Length != 10) {
+					newEndTimeStr += ".00";
+                }
+            }
+
+			string output = input;
+			output = output.Replace(startTimeStr, newStartTimeStr);
+			output = output.Replace(endTimeStr, newEndTimeStr);
+
+			return output;
+		}
+
+		public static void EditSubtitleFile(String filename, TimeSpan timeChange) {
+			string[] lines = File.ReadAllLines(filename);
+			using (StreamWriter file = new StreamWriter(Path.GetFileNameWithoutExtension(filename) + "n.ass")) {
+				foreach (string line in lines) {
+					file.WriteLine(EditOneLine(line, timeChange));
+				}
+			}
+		}
+
+		public static void EditSubtitleFiles(TimeSpan timeChange) {
+			string currentDirectory = Directory.GetCurrentDirectory();
+			string[] filenames = Directory.GetFiles(currentDirectory);
+			foreach (string filename in filenames) {
+				if (Path.GetExtension(filename) == ".ass") {
+					EditSubtitleFile(Path.Combine(currentDirectory, filename), timeChange);
+				}
+            }
+			Console.WriteLine("{0} files edited", filenames.Length);
+		}
+
+		public static string Truncate(string value, int maxLength) {
+			if (string.IsNullOrEmpty(value)) return value;
+			return value.Length <= maxLength ? value : value.Substring(0, maxLength);
+		}
+
+	}
+}
